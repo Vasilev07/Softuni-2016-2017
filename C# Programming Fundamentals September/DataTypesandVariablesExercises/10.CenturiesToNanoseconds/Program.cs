33d8e94f8c342cf1f7728bd6a974ad54c3a34bc6
George <george_vasilev07@abv.bg>
2017-02-07 17:07:36 +0200

    sss

--- C# Programming Fundamentals September/DataTypesandVariablesExercises/10.CenturiesToNanoseconds/Program.cs
+++ C# Programming Fundamentals September/DataTypesandVariablesExercises/10.CenturiesToNanoseconds/Program.cs
@@ -4,17 +4,16 @@ class Program
     static void Main(string[] args)
     {
         var centuries = int.Parse(Console.ReadLine());
-        var years = centuries * 100;
-        var days = (int)(Math.Ceiling((years * 365.24)));
-        var hours = days * 24;
-        var minutes = hours * 60;
-        var seconds = (ulong)(minutes * 60);
-        var miliseconds = (seconds * 1000);
-        var microseconds = miliseconds * 1000;
-        var nanoseconds = microseconds * 1000;
+        int years = centuries * 100;
+        int days = (int)(years * 365.2422);
+        int hours = days * 24;
+        int minutes = hours * 60;
+        decimal seconds = (decimal)minutes * 60;
+        decimal milliseconds = seconds * 1000;
+        decimal microseconds = milliseconds * 1000;
+        decimal nanoseconds = microseconds * 1000;
 
-        Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {miliseconds} milliseconds = {miliseconds} microseconds = {nanoseconds} nanoseconds");
-
+        Console.WriteLine($"{centuries:F0} centuries = {years:F0} years = {days:F0} days = {hours:F0} hours = {minutes:F0} minutes = {seconds:F0} seconds = {milliseconds:F0} milliseconds = {microseconds:F0} microseconds = {nanoseconds:F0} nanoseconds");
     }
 }
 
